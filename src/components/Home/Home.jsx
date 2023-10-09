7b5f742cdd56994140c1974657aa0f77dba1d510
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-10-09 17:29:03 +0600

    Started the project

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Sidenav from "../Pages/SideNav.jsx/Sidenav";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Sidenav></Sidenav>
+        </div>
+    );
+};
+
+export default Home;
